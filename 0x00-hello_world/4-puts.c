45703029de1392376e637cab5b873b1b29026f77
oluwafolayimika <oluwafolayimikabenson17@gmail.com>
2022-10-16 16:08:08 -0700

    Task 4-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -S $CFILE
+#include <stdio.h>
+/**
+ *  *main- Serves as the entry point for the program
+ *   *
+ *    *Return: By default returns zero if no erros otherwise returns non-zero value.
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
+
