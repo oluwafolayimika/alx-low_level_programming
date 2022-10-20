3582276212617550056161c12ab69ac0d735cbf2
oluwafolayimika <oluwafolayimikabenson17@gmail.com>
2022-10-20 09:41:07 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,12 @@
 #ifndef main_h
 #define main_h
-extern int_putchar(char c);
+extern int _putchar(char c);
 extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
-extern int_islower(int c);
-extern int_isalpha(int c);
+extern int _islower(int c);
+extern int _isalpha(int c);
 extern int print_sign(int n);
-extern int_abs(int);
+extern int _abs(int);
 extern int print_last_digit(int);
 extern void jack_bauer(void);
 extern void times_table(void);
