8b491af82f57e5f13f9a4884e085e09046a39caa
oluwafolayimika <oluwafolayimikabenson17@gmail.com>
2022-10-21 18:40:36 -0700

    Task 8 corrections

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_square - prints a square of size size
- * @size: size of the square 
+ * print_square - prints a square of size
+ * @size: size of the square
  *
  * Return: void
  */
