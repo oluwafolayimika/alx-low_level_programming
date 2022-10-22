886d2f5c90df41888d9905953c4c717570bd5f70
oluwafolayimika <oluwafolayimikabenson17@gmail.com>
2022-10-21 17:39:39 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,8 @@ void more_numbers(void)
 {
 
 	int i, j;
-	for (i = 1; i <= 10; i++)
-	{
+		for (i = 1; i <= 10; i++)
+		{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
@@ -19,5 +19,5 @@ void more_numbers(void)
 			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
-	}
+		}
 }
