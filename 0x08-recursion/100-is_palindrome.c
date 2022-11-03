a263c6bdde2175fbcbf0485094e62b42db511e2e
oluwafolayimika <oluwafolayimikabenson17@gmail.com>
2022-11-02 18:31:16 -0700

    corrections to optional task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -35,4 +35,5 @@ int pal_checker(char *s, int i, int j)
  */
 int is_palindrome(char *s)
 {
-	return (pal_checker(s, 0, _strlen_recursion(s) - 1));}
+	return (pal_checker(s, 0, _strlen_recursion(s) - 1));
+}
