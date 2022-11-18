1b07709b8410bee56644c8a71d82e305360b3b5f
oluwafolayimika <oluwafolayimikabenson17@gmail.com>
2022-11-17 18:11:18 -0800

    Corrections to task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,8 +10,9 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum;
 	unsigned int i;
-	
 	va_list ap;
+
+	va_start(ap, n);
 
 	if (n == 0)
 		return (0);
@@ -23,3 +24,5 @@ int sum_them_all(const unsigned int n, ...)
 
 	return (sum);
 }
+
+
