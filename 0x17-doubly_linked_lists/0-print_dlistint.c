821863ddfe32a49df8677aac15c58b151c51aff2
oluwafolayimika <oluwafolayimikabenson17@gmail.com>
2023-01-14 17:26:24 -0800

    correction

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * print_dlistint - print all the elements of a dlistint_t list
@@ -9,12 +8,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+	size_t num_nodes = 0;
 
-	for (i = 0; h != NULL; i++)
+	while (h)
 	{
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
+		num_nodes++;
 		h = h->next;
 	}
-	return (i);
+	return (num_nodes);
 }
